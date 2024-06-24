1fd708678775ae25b22b71ea2720831c1605f36d
aminsarshar <sarsharamin85@gmail.com>
2024-06-24 21:32:22 +0330

    add brands show and create

--- resources/views/admin/brands/create.blade.php
+++ resources/views/admin/brands/create.blade.php
@@ -1,52 +1,75 @@
 @extends('admin.layouts.admin')
-
 @section('title')
-    create brands
+ایجاد برند
 @endsection
-
 @section('content')
+    <section id="hidden-label-form-layouts">
 
-
-    <!-- Content Row -->
-    <div class="row">
-
-        <div class="col-xl-12 col-md-12 mb-4 p-4 bg-white">
-            <div class="mb-4 text-center text-md-right">
-                <h5 class="font-weight-bold">ایجاد برند</h5>
-            </div>
-            <hr>
-            @include('admin.sections.errors')
-            <form action="{{ route('admin.brands.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-
-                <div class="form-row">
-                    <div class="form-group col-md-3">
-                        <label for="name">نام</label>
-                        <input class="form-control" id="name" name="name" type="text" value="{{ old('name') }}" >
-                    </div>
-
-                    <div class="form-group col-md-3">
-                        <label for="image"> انتخاب تصویر اصلی </label>
-                        <div class="custom-file">
-                            <input type="file" name="image" class="custom-file-input" id="primary_image">
-                            <label class="custom-file-label" for="image"> انتخاب فایل </label>
+        <div class="row match-height">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title-wrap bar-success">
+                            <h4 class="card-title">ساخت برند</h4>
                         </div>
                     </div>
+                    <div class="card-body">
+                        <div class="px-3">
+                            <form class="form" novalidate method="POST" action="{{ route('admin.brands.store') }}"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                            <label class="sr-only" for="projectinput2">نام برند</label>
+                                            <input type="text" placeholder="نام برند" name="name" type="text" {{ old('name') }} class="form-control" required data-validation-required-message="فیلد نام برند الزامی است">
+                                            </div>
+                                        </div>
 
-                    <div class="form-group col-md-3">
-                        <label for="is_active">وضعیت</label>
-                        <select class="form-control" id="is_active" name="is_active">
-                            <option value="1" selected>فعال</option>
-                            <option value="0">غیرفعال</option>
-                        </select>
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                                <label class="sr-only" for="is_active">وضعیت</label>
+                                                <select class="form-control" id="is_active" name="is_active">
+                                                    <option value="1" selected>فعال</option>
+                                                    <option value="0">غیرفعال</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                                <label for="image"> انتخاب تصویر اصلی </label>
+                                                <div class="custom-file">
+                                                    <input type="file" name="image" class="custom-file-input" id="primary_image">
+                                                    <label class="custom-file-label" for="image"> انتخاب فایل </label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                    </div>
+
+
+                                </div>
+
+                                <div class="form-actions">
+                                    <button type="button" class="btn btn-danger mr-1">
+                                        <a class="text-white" href="{{ route('admin.brands.index') }}"><i class="icon-back"></i> بازگشت</a>
+                                    </button>
+                                    <button type="submit" class="btn btn-success">
+                                        <i class="icon-note"></i> ذخیره
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
+            </div>
 
-                <button class="btn btn-outline-primary mt-5" type="submit">ثبت</button>
-                <a href="{{ route('admin.brands.index') }}" class="btn btn-dark mt-5 mr-3">بازگشت</a>
-            </form>
         </div>
 
-    </div>
-
+    </section>
 @endsection
+
--- resources/views/admin/brands/show.blade.php
+++ resources/views/admin/brands/show.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layouts.admin')
+{{-- @extends('admin.layouts.admin')
 
 @section('title')
     show brands
@@ -6,7 +6,8 @@
 
 @section('content')
 
-    <!-- Content Row -->
+
+    {{-- <!-- Content Row -->
     <div class="row">
 
         <div class="col-xl-12 col-md-12 mb-4 p-4 bg-white">
@@ -35,6 +36,87 @@
 
         </div>
 
-    </div>
+    </div> --}}
 
+{{-- @endsection --}}
+
+@extends('admin.layouts.admin')
+@section('title')
+    نمایش برند
+@endsection
+@section('content')
+    <section id="hidden-label-form-layouts">
+
+        <div class="row match-height">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title-wrap bar-success">
+                            <h5 class="font-weight-bold"> نمایش برند {{ $brand->name }}</h5>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="px-3">
+                            <form class="form" novalidate method="POST" action="{{ route('admin.brands.store') }}"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                                <label class="" for="projectinput2">نام برند</label>
+                                                <input class="form-control" type="text" placeholder="نام برند"
+                                                    name="name" type="text" value="{{ $brand->name }}" disabled>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                                <label class="" for="projectinput2">تاریخ ایجاد</label>
+                                                <input class="form-control" type="text" placeholder="تاریخ ایجاد"
+                                                    name="name" type="text" value="{{ verta($brand->created_at)->format('%d  %B   %Y') }}" disabled>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                                <label class="" for="projectinput2">وضعیت برند</label>
+                                                <input class="form-control" type="text" value="{{ $brand->is_active }}" disabled>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group col-md-6 mb-2">
+                                            <div class="controls">
+                                                <label class="" for="projectinput2">لوگوی برند</label>
+                                                <br>
+                                                @if (!empty($brand->image))
+                                                <img src="{{ asset(env('BRAND_IMAGES_UPLOAD_PATH') . $brand->image) }}" alt="{{ $brand->name }}" style="width: 27%;height:88px;object-fit: fill;">
+                                                @else
+                                                <div class="alert alert-danger">لوگوی وجود ندارد</div>
+                                                @endif
+                                            </div>
+
+
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+
+                                <div class="form-actions">
+                                    <button type="button" class="btn btn-danger mr-1">
+                                        <a class="text-white" href="{{ route('admin.brands.index') }}"><i
+                                                class="icon-back"></i> بازگشت</a>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
 @endsection

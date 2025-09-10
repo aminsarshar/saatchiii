16bb4bdc6c99c01c40d13ea2073e7474fd2a39cf
aminsarshar <sarsharamin85@gmail.com>
2025-09-10 08:31:58 +0430

    delete js and css aditional file

--- resources/views/admin/sections/links_css.blade.php
+++ resources/views/admin/sections/links_css.blade.php
@@ -26,4 +26,3 @@
         href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    
